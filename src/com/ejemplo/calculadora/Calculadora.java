f28a81ee2e729b6b15c63b9bb548260aac18184c
JavierMartinGarcia <jmargar295w@g.educaand.es>
2025-10-23 11:52:22 +0200

    Añadida operación de resta

--- src/com/ejemplo/calculadora/Calculadora.java
+++ src/com/ejemplo/calculadora/Calculadora.java
@@ -7,8 +7,11 @@ public class Calculadora {
 
 	}
 	
-	double suma(double a, double b) {
+	public double suma(double a, double b) {
 		return a+b;
 	}
-
+	
+	public double restar(double a, double b) {
+		return a - b;
+		}
 }
