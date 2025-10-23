9ca13c878b9b7975c3b74dba65200671a4bb1ca6
JavierMartinGarcia <jmargar295w@g.educaand.es>
2025-10-23 11:14:13 +0200

    Versión inicial: clase Calculadora con operación de suma

---
+++ src/com/ejemplo/calculadora/Calculadora.java
@@ -0,0 +1,14 @@
+package com.ejemplo.calculadora;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	double suma(double a, double b) {
+		return a+b;
+	}
+
+}
